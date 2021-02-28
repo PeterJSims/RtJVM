31683eb83b9e7ae98366fa003a5a4b1d8565be82
Peter Sims <ragtimesims@gmail.com>
2021-02-28 16:56:10 -0500

    monad notes and exercises

---
+++ Advanced/src/lectures/part2afp/Monads.scala
@@ -0,0 +1,61 @@
+package lectures.part2afp
+
+object Monads extends App {
+
+
+  // our own version of Try
+  trait Attempt[+A] {
+    def flatMap[B](f: A => Attempt[B]): Attempt[B]
+  }
+
+  object Attempt {
+    def apply[A](a: => A): Attempt[A] =
+      try {
+        Success(a)
+      } catch {
+        case e: Throwable => Fail(e)
+      }
+  }
+
+  case class Success[+A](value: A) extends Attempt[A] {
+    override def flatMap[B](f: A => Attempt[B]): Attempt[B] =
+      try {
+        f(value)
+      } catch {
+        case e: Throwable => Fail(e)
+      }
+
+  }
+
+  case class Fail(e: Throwable) extends Attempt[Nothing] {
+    override def flatMap[B](f: Nothing => Attempt[B]): Attempt[B] = this
+  }
+
+  // Lazy monad
+  class Lazy[+A](value: => A) { // remember to call by name when constructing lazy values
+    // call by need
+    private lazy val internalValue = value
+    def use: A = internalValue
+
+    def flatMap[B](f: (=> A) => Lazy[B]): Lazy[B] = f(internalValue)
+  }
+
+  object Lazy {
+    def apply[A](value: => A): Lazy[A] = new Lazy(value)
+  }
+
+  val lazyInstance = Lazy {
+    println("I'm being lazy")
+    42
+  }
+
+  val flatMappedInstance = lazyInstance.flatMap(x => Lazy {
+    10 * x
+  })
+  val flatMappedInstance2 = lazyInstance.flatMap(x => Lazy {
+    10 * x
+  })
+  flatMappedInstance.use
+  flatMappedInstance2.use
+}
+
